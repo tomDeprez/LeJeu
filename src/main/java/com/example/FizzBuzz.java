feaafe6b602ba8b21b39a0474cc25d60e9cb7e18
Tom DEPREZ <tom_deprez@hotmail.fr>
2025-02-26 13:33:22 +0000

    new maj

--- src/main/java/com/example/FizzBuzz.java
+++ src/main/java/com/example/FizzBuzz.java
@@ -5,20 +5,4 @@ import java.util.Map;
 
 public class FizzBuzz {
 
-    private final Map<Integer, String> fizzBuzzMap = new HashMap<Integer, String>() {{
-        put(3, "Fizz");
-        put(5, "Buzz");
-    }};
-
-    public String build(int input) {
-        StringBuilder result = new StringBuilder();
-
-        fizzBuzzMap.forEach((key, value) -> {
-            if (input % key == 0) {
-                result.append(value);
-            }
-        });
-
-        return result.isEmpty() ? String.valueOf(input) : result.toString();
-    }
 }
--- src/test/java/com/example/FizzBuzzTest.java
+++ src/test/java/com/example/FizzBuzzTest.java
@@ -7,51 +7,5 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 4})
-    public void getNumberWhenNotModulo3And5(int input) {
-      String result = String.valueOf(input);
-      FizzBuzz fb = new FizzBuzz();
-  
-      assertEquals(result, fb.build(input));
-    }
 
-    @Test
-    public void getFizzWithModulo3(){
-      int imput = 3;
-      String result = "Fizz";
-
-      FizzBuzz fb = new FizzBuzz();
-
-      assertEquals(result, fb.build(imput));
-    }
-
-    @Test
-    public void getFizzWithModulo6(){
-      int imput = 6;
-      String result = "Fizz";
-
-      FizzBuzz fb = new FizzBuzz();
-
-      assertEquals(result, fb.build(imput));
-    }
-
-    @Test
-    public void getBuzzWithModulo5(){
-      int imput = 5;
-      String result = "Buzz";
-
-      FizzBuzz fb = new FizzBuzz();
-
-      assertEquals(result, fb.build(imput));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {15, 30, 45})
-    public void getNumberWhenModuloFizzAndBuzz(int input) {
-      String result = "FizzBuzz";
-      FizzBuzz fb = new FizzBuzz();
-  
-      assertEquals(result, fb.build(input));
-    }
 }
